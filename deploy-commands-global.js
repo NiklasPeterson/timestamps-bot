ed881e8a12098e38ac812acd026343e3fe86791f
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2022-11-26 22:03:24 +0100

    adjusting deply-commands

--- deploy-commands-global.js
+++ deploy-commands-global.js
@@ -1,14 +1,6 @@
 const { REST, Routes } = require('discord.js');
+const { clientId, token } = require('./config.json');
 const fs = require('node:fs');
-
-
-const dotenv = require('dotenv')
-// import config IDs
-dotenv.config()
-
-const TOKEN = process.env.TOKEN
-const CLIENT_ID = process.env.CLIENT_ID
-const GUILD_ID = process.env.GUILD_ID
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
@@ -21,7 +13,7 @@ for (const file of commandFiles) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(TOKEN);
+const rest = new REST({ version: '10' }).setToken(token);
 
 // and deploy your commands!
 (async () => {
@@ -30,7 +22,7 @@ const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
