90539318150a7935bf0312859618060e553ebcf1
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2022-11-26 22:20:27 +0100

    updateing the deply-commands to deploy again

--- deploy-commands.js
+++ deploy-commands.js
@@ -31,7 +31,7 @@ const rest = new REST({ version: '10' }).setToken(TOKEN);
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
 			Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-			{ body: [] },
+			{ body: [commands] },
 		);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
