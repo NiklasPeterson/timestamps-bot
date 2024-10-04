b6330d3d2d77b12dbb1d40a3b7e6f3a838262c49
NiklasPeterson <5664205+NiklasPeterson@users.noreply.github.com>
2024-10-04 17:42:29 +0200

    adds code to send a message in my log channel when the bot awakens

--- events/ready.js
+++ events/ready.js
@@ -6,6 +6,15 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		const channelId = process.env.LOG_CHANNEL_ID;
+		const channel = client.channels.cache.get(channelId);
+		if (channel) {
+			channel.send(`I'm awake!`);
+		}
+		else {
+			console.error('Channel not found!');
+		}
+
 		client.user.setActivity(`Used in ${client.guilds.cache.size} servers`, { type: ActivityType.Custom });
 	},
 };
