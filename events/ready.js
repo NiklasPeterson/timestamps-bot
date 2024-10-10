fa6be4adced8271c930dcf2321b1e9278f266151
NiklasPeterson <5664205+NiklasPeterson@users.noreply.github.com>
2024-10-10 15:55:42 +0200

    removes awake logging

--- events/ready.js
+++ events/ready.js
@@ -8,17 +8,6 @@ module.exports = {
 	execute(client) {
 		try {
 			console.log(`Ready! Logged in as ${client.user.tag}`);
-
-			// Send a message to a specific log channel (if exists)
-			const channelId = process.env.LOG_CHANNEL_ID;
-			const channel = client.channels.cache.get(channelId);
-			if (channel) {
-				channel.send('I\'m awake!');
-			}
-			else {
-				console.error('Log channel not found!');
-			}
-
 			// Update bot's activity with error handling
 			updateBotActivity(client);
 
