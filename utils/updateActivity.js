d0c99e4a4eeb84068aaaf1309c87828c3d5d0fc2
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2025-07-31 22:28:04 +0200

    switches SUPABASE_ANON_KEY to SUPABASE_SERVICE_ROLE_KEY

--- utils/updateActivity.js
+++ utils/updateActivity.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // Initialize Supabase with optimized options
 const supabase = createClient(
 	process.env.SUPABASE_URL,
-	process.env.SUPABASE_ANON_KEY,
+	process.env.SUPABASE_SERVICE_ROLE_KEY
 	{
 		auth: { persistSession: false },
 		db: { schema: 'public' }
